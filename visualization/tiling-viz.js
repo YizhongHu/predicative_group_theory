31a197bcdfeeb9a497ace6779dda260ce8cabaab
Ian Benway <ian.benway@gmail.com>
2022-04-24 21:19:44 -0400

    put vizs in folder, add example imgs

--- visualization/tiling-viz.js
+++ visualization/tiling-viz.js
@@ -5,7 +5,45 @@ d3.selectAll("svg > *").remove();
 
 // Can display elements as letters or colors
 const COLOR_SCHEME = "pastel"; //"normal", "pastel", "ruby", "sandstone"
+const SCALE = 1
 
+
+RED2 = "";
+ORANGE2 = "";
+YELLOW2 = "";
+GREEN2 = "";
+BLUE2 = "";
+INDIGO2 = "";
+VIOLET2 = "";
+PINK2 = "";
+if(COLOR_SCHEME == "normal") {
+    RED = "#df4772";
+    ORANGE = "#ff9500";
+    YELLOW = "#f6ff00";
+    GREEN = "#d5f2d5";
+    BLUE = "#1ee7f7";
+    INDIGO = "#1e2df7";
+    VIOLET = "#bb0fe2";
+    PINK = "#ff0ba1";
+}
+if(COLOR_SCHEME == "sixteen") {
+    RED = "#00ffc8";
+    ORANGE = "#00f8c8";
+    YELLOW = "#00f0d0";
+    GREEN = "#00ead8";
+    BLUE = "#00e2d8";
+    INDIGO = "#00dad8";
+    VIOLET = "#00d3e0";
+    PINK = "#00cae0";
+    RED2 = "#00c5e7";
+    ORANGE2 = "#00bee7";
+    YELLOW2 = "#00b6ef";
+    GREEN2 = "#00b0ef";
+    BLUE2 = "#00a8f7";
+    INDIGO2 = "#0083b9";
+    VIOLET2 = "#bb0fe2";
+    PINK2 = "#9818b4";
+}
 if(COLOR_SCHEME == "normal") {
     RED = "#df4772";
     ORANGE = "#ff9500";
@@ -65,18 +103,19 @@ function intToABC(int) {
 }
 
 function intToColor(int) {
-    colors = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, PINK];
+    colors = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, PINK,
+        RED2, ORANGE2, YELLOW2, GREEN2, BLUE2, INDIGO2, VIOLET2, PINK2];
     return colors[int]
 }
 
 function printRect(row, col, yoffset, xoffset, value) {
     d3.select(svg)
     .append('rect')
-    .attr('x', (row)*30 + xoffset)
-    .attr('y', (col)*30 + yoffset)
-    .attr('width', 30)
-    .attr('height', 30)
-    .attr('stroke-width', 2)
+    .attr('x', (row)*30*SCALE + xoffset)
+    .attr('y', (col)*30*SCALE + yoffset)
+    .attr('width', 30*SCALE)
+    .attr('height', 30*SCALE)
+    .attr('stroke-width', 2*SCALE)
     .attr('fill', value);
 }
 
@@ -86,9 +125,9 @@ function printGroupUL(groupAtom, yoffset, xoffset) {
         for (c = 0; c <= numEls; c++) {
           if(Element.atom("Element"+r) != null) {
             if(Element.atom("Element"+c) != null) {
-                val = intToColor(groupAtom
+                var val = intToColor(groupAtom
                     .table[Element.atom("Element"+r)][Element.atom("Element"+c)]
-                    .toString().slice(-1))
+                    .toString().slice(-1));
                 printRect(r, c, yoffset, xoffset, val)
             }
           }
@@ -105,7 +144,7 @@ function printGroupLL(groupAtom, yoffset, xoffset) {
                 val = intToColor(groupAtom
                     .table[Element.atom("Element"+r)][Element.atom("Element"+c)]
                     .toString().slice(-1))
-                printRect(numEls - r - 1, c, yoffset, xoffset, val)
+                printRect(r, numEls - c - 1, yoffset, xoffset, val)
             }
           }
         }
@@ -121,7 +160,7 @@ function printGroupUR(groupAtom, yoffset, xoffset) {
                 val = intToColor(groupAtom
                     .table[Element.atom("Element"+r)][Element.atom("Element"+c)]
                     .toString().slice(-1))
-                printRect(r, numEls-c-1, yoffset, xoffset, val)
+                printRect(numEls-r-1, c, yoffset, xoffset, val)
             }
           }
         }
@@ -148,9 +187,9 @@ function printGroupLR(groupAtom, yoffset, xoffset) {
 
 var yoffset = 0
 var xoffset = 0
-for(b = 0; b <= 5; b++) {  
+for(b = 0; b <= 2*(1/SCALE); b++) {  
     xoffset = 0
-    for(t = 0; t <= 5; t++){
+    for(t = 0; t <=2*(1/SCALE); t++){
         // upper left
         if(b % 2 == 0 && t % 2 == 0)
             printGroupUL(Group.atom("Group0"), yoffset, xoffset) 
@@ -165,8 +204,8 @@ for(b = 0; b <= 5; b++) {
             printGroupLR(Group.atom("Group0"), yoffset, xoffset) 
 
 
-        xoffset = xoffset + 30 * numEls
+        xoffset = xoffset + 30 * numEls * SCALE
     }
-  yoffset = yoffset + 30 * numEls
+  yoffset = yoffset + 30 * numEls * SCALE
   xoffset = 0
 }
